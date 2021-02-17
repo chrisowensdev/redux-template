e15720e5e16f0e43a799926b31ad77c5117a7597
Chris Owens <guitarrockstarco@gmail.com>
2021-02-17 11:28:24 -0500

    Setup index with starter redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// components
+import App from './App';
+import Hooks from './components/Hooks';
+import Classes from './components/Classes';
+import BaseLayout from './components/layout/BaseLayout';
+
+// redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// initializing redux store
+// requires a reducer.  Second argument is for redux dev-tools extension.
+let store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//provider connects react to redux.  Must pass redux instance to provider via "store" prop.
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <BaseLayout>
+                    <Switch>
+                        <Route exact path='/' component={App} />
+                        <Route path='/hooks' component={Hooks} />
+                        <Route path='/classes' component={Classes} />
+                    </Switch>
+                </BaseLayout>
+            </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
